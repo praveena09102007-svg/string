ed4ca46cf66ff7b952789165eb60a63090f43246
praveena09102007-svg <praveena09102007@gmail.com>
2026-03-13 16:03:58 +0530

    Add files via upload

---
+++ 15 Word Counter in Text Editor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, count = 1;
+
+   
+    fgets(str, 100, stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == ' ')
+        {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
